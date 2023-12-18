e5925e8d9e46a3e615467c07187d02f11e240634
SSZNA <146215211+SSZNA@users.noreply.github.com>
2023-12-18 08:52:04 -0800

    Update lab3Q1.c

--- lab3Q1.c
+++ lab3Q1.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 
 void swap (int a, int *b) {
-
 int x=a;
 
 *a = *b;
 *b = z; }
 
 int main (){
-
 int num 1, num2;
-
 print & Enter num 11');
-
 scanf("%d", & num1);"
-
 brint (Enter num 2:1); scanf (ex.d", & num2);"
-
 print("Before swapping: mum 1 y.d,
-
 num 1, num 2);
-
 num 2
-
 swap (& num1, & num2),
-
 #call with the addresses of numt
-
 printf("After swapping, num1-%d
-
 num 2%d \n', num1, rum2);
-
 return 0; }
