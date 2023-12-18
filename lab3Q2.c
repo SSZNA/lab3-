1dbbaee99c48d99c496c9b9fdfb0376a3559f80b
SSZNA <146215211+SSZNA@users.noreply.github.com>
2023-12-18 21:50:17 +0500

    Add files via upload

---
+++ lab3Q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main(){
+char string [100];
+printf("Enter a string:");
+scanf("%s",&string);
+char *ptr = string + strlen(string)-1;
+printf("Reversed string:");
+while ( ptr >= string) {
+printf("%c",*ptr);
+ptr--;}
+printf("\n");
+return 0; }
