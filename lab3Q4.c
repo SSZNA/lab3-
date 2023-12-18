e57f12dabf4bb7d85d3d17b4d3e4ab38c6a3aca2
SSZNA <146215211+SSZNA@users.noreply.github.com>
2023-12-18 22:12:31 +0500

    Add files via upload

---
+++ lab3Q4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+int arr[] = {1,2,3,4,5};
+int n = sizeof(arr);
+int target;
+printf("Enter the element to search: ");
+scanf("%d", &target);
+int * ptr = arr;
+int found = 0;
+
+for (int i=0; i<n; i++) {
+if (*ptr == target) {
+
+found = 1;
+break; }
+ptr ++; }
+if (found){
+
+printf("%d is found in the array \n",target);
+} else { printf ("%d is not found in the array \n", target);
+}
+return 0;
+}
